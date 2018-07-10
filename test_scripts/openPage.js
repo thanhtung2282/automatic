2381fa364d18057ecdb0e2b9aa4435e9703b96c6
thanhtung2282 <tungnguyenthanh357@gmail.com>
2018-07-10 18:50:37 +0700

    waitForVisible

--- test_scripts/openPage.js
+++ test_scripts/openPage.js
@@ -1,15 +1,12 @@
 describe('API Command',()=>{
     it('Open Page API',()=>{
         //B1 : mở page google
-
         browser.url('https://google.com');
-        browser.pause(3000);
 
         //B2 setValue "AUTOMATION TEST" into field search of gg
 
-        $('#lst-ib').setValue('AUTOMATION TEST');
-        // browser.setValue('#lst-ib','TEST');
-        browser.pause(3000);
-        
+        let SEARCH_TXTBX = '#lst-ib';
+        browser.waitForVisible(SEARCH_TXTBX,3000)
+        $(SEARCH_TXTBX).setValue('AUTOMATION');
     })
 });
