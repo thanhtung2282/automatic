ff7c9ee363f892983db650e03b3edb2eee33f632
thanhtung2282 <tungnguyenthanh357@gmail.com>
2018-07-10 23:28:38 +0700

    getText

--- test_scripts/Alert.js
+++ test_scripts/Alert.js
@@ -1,24 +1,26 @@
 describe('API COMMAND',()=>{
     const JS_ALERT='[onclick="jsConfirm()"]';
+    const RESULT='#result';
     browser.url('https://the-internet.herokuapp.com/javascript_alerts');    
     it('ALERT ACCEPT',()=>{
         browser.waitForVisible(JS_ALERT,10000);
         $(JS_ALERT).click();
-        browser.pause(3000);        
+    
         browser.alertAccept();
-        browser.pause(3000);
+        let result = $(RESULT).getText();
+        console.log(result) //You clicked: Ok
     });
     it('ALERT DISMISS',()=>{
         $(JS_ALERT).click();
-        browser.pause(3000);        
+      
         browser.alertDismiss();
-        browser.pause(3000);
+
     });
     it('ALERT TEXT',()=>{
         $(JS_ALERT).click();
-        browser.pause(3000);        
+      
         let alert = browser.alertText();
         console.log(alert)
-        browser.pause(3000);
+
     });
 });
