ea33407c1aff836c91af85a73c4e375c9250e83f
thanhtung2282 <tungnguyenthanh357@gmail.com>
2018-07-12 10:39:50 +0700

    befor and after

--- pageObject/test_scripts/LoginFormActionChain.js
+++ pageObject/test_scripts/LoginFormActionChain.js
@@ -1,18 +1,23 @@
 const LoginPage = require('../LoginPageActionChain');
 const { equal } = require('assert');
 describe('LOGIN PAGE ACTION CHAIN', () => {
+    let pageUrl = 'https://the-internet.herokuapp.com/login';
+    before('open page for test',()=>{
+        LoginPage.openPage(pageUrl);
+        console.log('open page')
+        
+    })
     it('LOGIN FORM SUCCESS', () => {
         let username = 'tomsmith';
         let password = 'SuperSecretPassword!';
-        let pageUrl = 'https://the-internet.herokuapp.com/login';
-
-
         result = LoginPage
-            .openPage(pageUrl)
             .inputUserName(username)
             .inputPassword(password)
             .btnSubmit()
             .getText();
         equal(result, 'Secure Area')
+    });
+    after('Clear data after test case',()=>{
+        console.log('Clear data')
     });
 });
