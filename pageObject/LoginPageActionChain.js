f791fc6abe9790fd58084772f36539d111f2ca5e
thanhtung2282 <tungnguyenthanh357@gmail.com>
2018-07-12 14:51:06 +0700

    tach module kiemtra and test

--- pageObject/LoginPageActionChain.js
+++ pageObject/LoginPageActionChain.js
@@ -1,11 +1,12 @@
 const Base = require('./Base');
+const { expect  } = require('chai');
 const SecureAreaPage = require('./SecureAreaPage');
 const USERNAME_TXT = '#username';
 const PASSWORD_TXT = '#password';
 const LOGIN_BTN = '[type="submit"]';
 const URL_SUCCESS = 'https://the-internet.herokuapp.com/secure';
 const URL_ERROR = 'https://the-internet.herokuapp.com/login';
-
+const LOGIN_TITLE ='h2'
 class LoginPage extends Base {
     
     inputUserName(username){
@@ -20,6 +21,15 @@ class LoginPage extends Base {
     btnSubmit(){
         $(LOGIN_BTN).click();
         return SecureAreaPage;
+        
+    }
+    getLoginTitle(){
+        return $(LOGIN_TITLE).getText();
+    }
+    verifyTitle(){
+        let title = this.getLoginTitle();
+        let expectedTitle = 'Login Page@';
+        expect(title).to.equal(expectedTitle,'ERR: Login Title Wrong');
         
     }
 }
--- pageObject/test_scripts/LoginFormActionChain.js
+++ pageObject/test_scripts/LoginFormActionChain.js
@@ -1,6 +1,5 @@
 const LoginPage = require('../LoginPageActionChain');
 const dataLoginPage = require('../test_Data/LoginPage.json');
-const { expect  } = require('chai');
 describe('LOGIN PAGE ACTION CHAIN', () => {
     let pageUrl = 'https://the-internet.herokuapp.com/login';
     before('open page for test',()=>{
@@ -9,20 +8,14 @@ describe('LOGIN PAGE ACTION CHAIN', () => {
     beforeEach('refesh for test',()=>{
         browser.refresh();
     })
-    it('LOGIN FORM FAIL', () => {
-        result = LoginPage
-            .inputUserName(dataLoginPage.incorrectCredential.username)
-            .inputPassword(dataLoginPage.incorrectCredential.password)
-            .btnSubmit()
-            browser.pause(2000)
+    it('CHECK LOGIN TITLE', () => {
+        LoginPage.verifyTitle();
     });
     it('LOGIN FORM SUCCESS', () => {
         result = LoginPage
             .inputUserName(dataLoginPage.correctCredential.username)
             .inputPassword(dataLoginPage.correctCredential.password)
             .btnSubmit()
-            .getText();
-        expect(result).to.be.a('string','ERR: Result not a string');
     });
     after('Clear data after test case',()=>{
         console.log('Clear data')
